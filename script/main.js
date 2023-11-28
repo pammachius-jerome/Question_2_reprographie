d5de09ffe444b121e982fe8c86abc20e4ac49a90
Pammachius Jérôme <scipions.prof@free.fr>
2023-11-28 11:43:29 +0100

    first commit

---
+++ script/main.js
@@ -0,0 +1,21 @@
+// création des variables global
+let nombrePhotocopies;
+let prixPhotocopies; 
+let bouton = document.getElementById("bouton");
+let message = document.getElementById("message");
+
+// fonction de traitement 
+
+function calcul() {
+    nombrePhotocopies = document.getElementById("valeurSaisie").value;
+    if (nombrePhotocopies <= 10) {
+        prixPhotocopies = nombrePhotocopies * 0.1;
+    } else if (nombrePhotocopies <= 30) {
+        prixPhotocopies = (nombrePhotocopies -10) * 0.09;
+        
+    }
+}
+
+// écoute de l'envoie
+
+bouton.addEventListener("click", calcul, false);
